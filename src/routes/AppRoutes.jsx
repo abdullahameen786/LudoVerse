5b828d585e870d1185efababb44ca55ba02a6039
abdullahameen786 <abdullahameen2k22@gmail.com>
2026-08-03 01:22:23 +0500

    Add protected route authentication

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,36 +1,10 @@
-import { Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./ProtectedRoute";
 
-import MainLayout from "../layouts/MainLayout";
-
-import Home from "../pages/Home";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
-import Game from "../pages/Game";
-import NotFound from "../pages/NotFound";
-
-function AppRoutes() {
-  return (
-    <Routes>
-
-      <Route element={<MainLayout />}>
-
-        <Route path="/" element={<Home />} />
-
-        <Route path="/login" element={<Login />} />
-
-        <Route path="/register" element={<Register />} />
-
-        <Route path="/game" element={<Game />} />
-
-      </Route>
-
-      <Route
-        path="*"
-        element={<NotFound />}
-      />
-
-    </Routes>
-  );
-}
-
-export default AppRoutes;
+<Route
+  path="/game"
+  element={
+    <ProtectedRoute>
+      <Game />
+    </ProtectedRoute>
+  }
+/>
